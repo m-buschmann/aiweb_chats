6afae31e0593e7125977bd49c9046f37b6b39732
m-buschmann <snep114@gmail.com>
2024-02-03 19:52:39 +0100

    new trial

--- channel2.py
+++ channel2.py
@@ -24,7 +24,7 @@ HUB_AUTHKEY = 'Crr-K3d-2N'
 
 CHANNEL_AUTHKEY = '22334455'
 CHANNEL_NAME = "The Noisy Channel"
-CHANNEL_ENDPOINT = "http://vm954.rz.uni-osnabrueck.de/user031/channel2"
+CHANNEL_ENDPOINT = "http://vm954.rz.uni-osnabrueck.de/user031/channel2.wsgi"
 CHANNEL_FILE = 'messages2.json'
 
 @app.cli.command('register')
